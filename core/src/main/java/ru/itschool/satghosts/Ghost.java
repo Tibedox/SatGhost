bee188f466f074117d9cc29e1b9a05a4daf69721
teacher <tibedox@mail.ru>
2026-03-14 17:34:16 +0600

    Ghost move

---
+++ core/src/main/java/ru/itschool/satghosts/Ghost.java
@@ -0,0 +1,33 @@
+package ru.itschool.satghosts;
+
+import static ru.itschool.satghosts.Main.SCR_HEIGHT;
+import static ru.itschool.satghosts.Main.SCR_WIDTH;
+
+import com.badlogic.gdx.math.MathUtils;
+
+public class Ghost {
+    float x, y;
+    float width, height;
+    float vx, vy;
+
+    public Ghost() {
+        width = MathUtils.random(70, 250);
+        height = width*1.756f;
+        x = MathUtils.random(0, SCR_WIDTH-width);
+        y = MathUtils.random(0, SCR_HEIGHT-height);
+        vx = MathUtils.random(-5f, 5f);
+        vy = MathUtils.random(-5f, 5f);
+    }
+
+    void move() {
+        x += vx;
+        y += vy;
+
+        if(x<0 || x>SCR_WIDTH-width){
+            vx = -vx;
+        }
+        if(y<0 || y>SCR_HEIGHT-height){
+            vy = -vy;
+        }
+    }
+}
--- core/src/main/java/ru/itschool/satghosts/Main.java
+++ core/src/main/java/ru/itschool/satghosts/Main.java
@@ -6,20 +6,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
-    final float SCR_WIDTH = 1600, SCR_HEIGHT = 900;
+    static final float SCR_WIDTH = 1600, SCR_HEIGHT = 900;
     SpriteBatch batch;
     Texture imgBackGround;
+    Texture imgGhost;
+    Ghost ghost;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         imgBackGround = new Texture("grave.png");
+        imgGhost = new Texture("ghost.png");
+        ghost = new Ghost();
     }
 
     @Override
     public void render() {
+        // события
+        ghost.move();
+
+        // отрисовка
         batch.begin();
         batch.draw(imgBackGround, 0, 0, SCR_WIDTH, SCR_HEIGHT);
+        batch.draw(imgGhost, ghost.x, ghost.y, ghost.width, ghost.height);
         batch.end();
     }
 
@@ -27,5 +36,6 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         imgBackGround.dispose();
+        imgGhost.dispose();
     }
 }
