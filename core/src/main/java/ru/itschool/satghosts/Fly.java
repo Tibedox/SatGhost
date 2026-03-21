0b2f8eb9b5ff400240d5f1226178fb436f2d344b
teacher <tibedox@mail.ru>
2026-03-21 16:12:02 +0600

    extends

--- core/src/main/java/ru/itschool/satghosts/Fly.java
+++ core/src/main/java/ru/itschool/satghosts/Fly.java
@@ -5,10 +5,7 @@ import static ru.itschool.satghosts.Main.SCR_WIDTH;
 
 import com.badlogic.gdx.math.MathUtils;
 
-public class Fly {
-    float x, y;
-    float width, height;
-    float vx, vy;
+public class Fly extends Ghost{
 
     public Fly() {
         width = MathUtils.random(20, 50);
@@ -17,17 +14,5 @@ public class Fly {
         y = MathUtils.random(0, SCR_HEIGHT-height);
         vx = MathUtils.random(-15f, 15f);
         vy = MathUtils.random(-15f, 15f);
-    }
-
-    void move() {
-        x += vx;
-        y += vy;
-
-        if(x<0 || x>SCR_WIDTH-width){
-            vx = -vx;
-        }
-        if(y<0 || y>SCR_HEIGHT-height){
-            vy = -vy;
-        }
     }
 }
